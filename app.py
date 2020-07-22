140f8a62cd50aa3d871550f08ef49c65f1c713be
dezerr <dezerrpro@gmail.com>
2020-07-23 01:02:03 +0200

    program translation

--- app.py
+++ app.py
@@ -10,29 +10,29 @@ try:
     print("-----------------------------")
     print("Tool made by dezerr")
     print("-----------------------------\n")
-    numberOfPlayer = input("Quels est le nombre de joueurs ? : ")
+    numberOfPlayer = input("What is the number of players ? : ")
     numberOfPlayer = int(numberOfPlayer)
     
 
     def shuffler():
         i = 0
         iPlusOne = i + 1
-        # Tableau qui contient tous les joueurs
+        # Table containing all the players
         arrNameOfPLayer = []
 
         while i < numberOfPlayer:
-            nameOfPlayer = input("Quel est le nom du joueur " + str(iPlusOne)  + " ? : ")
+            nameOfPlayer = input("What's the name of the player " + str(iPlusOne)  + " ? : ")
             arrNameOfPLayer.append(nameOfPlayer)
             i = i + 1
             iPlusOne = iPlusOne + 1
 
-        print("\nLes joueurs sont : " + str(arrNameOfPLayer))
+        print("\nThe players are : " + str(arrNameOfPLayer))
         random.shuffle(arrNameOfPLayer)
-        print("\nLes joueurs ont été mélanger aléatoirement : " + str(arrNameOfPLayer) + "\n")
+        print("\nThe players were randomly shuffled : " + str(arrNameOfPLayer) + "\n")
 
 
         # Séparation d'équipes
-        numberOfTeam = input("Combien d’équipes voulez vous ? : ")
+        numberOfTeam = input("How many teams do you want ? : ")
         numberOfTeam = int(numberOfTeam)
         test = numberOfPlayer % numberOfTeam
 
@@ -40,20 +40,20 @@ try:
             pad = len(arrNameOfPLayer) / numberOfTeam
             pad = int(pad)
             aS = [arrNameOfPLayer[i:i+pad]for i in range(0, len(arrNameOfPLayer) - pad + 1, pad)]
-            print("Voici les " + str(numberOfTeam) + " équipes : \n")
+            print("Here are the " + str(numberOfTeam) + " teams : \n")
             print(aS)
             print("\n")
         else:
-            print("Ce n'est pas possible")
+            print("That's not possible.")
         
 
     if numberOfPlayer <= 2:
-            print("Vous ne pouvez pas être moin de 2 joueur")
+            print("You cannot be less than 2 players")
     elif numberOfPlayer in range(2,21):
             shuffler()
 
     elif numberOfPlayer > 20:
-            print("Vous ne pouvez pas être plus de 20 joueurs")
+            print("You can't be more than 20 players")
 
 except:
-    print("Erreur.")
+    print("Error.")
